d255279f5afdd48af1d76ebc92790387143f9618
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-09 22:42:47 +0300

    Mount exact location manager

--- mobile/index.js
+++ mobile/index.js
@@ -3,7 +3,8 @@ import {View,StyleSheet} from 'react-native';
 import {registerRootComponent} from 'expo';
 import App from './App';
 import ExploreOverlay from './src/ExploreOverlay';
+import LocationManager from './src/LocationManager';
 
-function Root(){return <View style={s.root}><App/><ExploreOverlay/></View>}
+function Root(){return <View style={s.root}><App/><ExploreOverlay/><LocationManager/></View>}
 const s=StyleSheet.create({root:{flex:1}});
 registerRootComponent(Root);
