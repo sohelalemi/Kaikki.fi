976a2be3e47750154e86f78b2f425c089251bb69
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-09 22:47:36 +0300

    Mount push notification registrar

--- mobile/index.js
+++ mobile/index.js
@@ -4,7 +4,8 @@ import {registerRootComponent} from 'expo';
 import App from './App';
 import ExploreOverlay from './src/ExploreOverlay';
 import LocationManager from './src/LocationManager';
+import PushRegistrar from './src/PushRegistrar';
 
-function Root(){return <View style={s.root}><App/><ExploreOverlay/><LocationManager/></View>}
+function Root(){return <View style={s.root}><App/><ExploreOverlay/><LocationManager/><PushRegistrar/></View>}
 const s=StyleSheet.create({root:{flex:1}});
 registerRootComponent(Root);
