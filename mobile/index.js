305ae1f6e5dc4d42f7897b54a796929595a5562a
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-10 08:31:12 +0300

    Wire advanced sell flow into mobile root

--- mobile/index.js
+++ mobile/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import ExploreOverlay from './src/ExploreOverlay';
 import LocationManager from './src/LocationManager';
 import PushRegistrar from './src/PushRegistrar';
+import AdvancedSellManager from './src/AdvancedSellManager';
 
-function Root(){return <View style={s.root}><App/><ExploreOverlay/><LocationManager/><PushRegistrar/></View>}
+function Root(){return <View style={s.root}><App/><ExploreOverlay/><LocationManager/><AdvancedSellManager/><PushRegistrar/></View>}
 const s=StyleSheet.create({root:{flex:1}});
 registerRootComponent(Root);
