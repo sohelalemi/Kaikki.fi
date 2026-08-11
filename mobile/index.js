9597b5183b083b5128a3267e2dacde9d28879f6f
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-11 23:48:16 +0300

    Stabilize Expo Go app entry

--- mobile/index.js
+++ mobile/index.js
@@ -1,12 +1,4 @@
-import React from 'react';
-import {View,StyleSheet} from 'react-native';
-import {registerRootComponent} from 'expo';
+import { registerRootComponent } from 'expo';
 import App from './App';
-import ExploreOverlay from './src/ExploreOverlay';
-import LocationManager from './src/LocationManager';
-import PushRegistrar from './src/PushRegistrar';
-import AdvancedSellManager from './src/AdvancedSellManager';
 
-function Root(){return <View style={s.root}><App/><ExploreOverlay/><LocationManager/><AdvancedSellManager/><PushRegistrar/></View>}
-const s=StyleSheet.create({root:{flex:1}});
-registerRootComponent(Root);
+registerRootComponent(App);
