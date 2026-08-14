00207de0a1755b29665a7e90176903f9c191f97d
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-14 23:49:23 +0300

    Force mobile app to use Kaikki.fi Supabase project

--- mobile/src/supabase.js
+++ mobile/src/supabase.js
@@ -1,9 +1,9 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { validateEnv } from './env';
 
-const { EXPO_PUBLIC_SUPABASE_URL: url, EXPO_PUBLIC_SUPABASE_ANON_KEY: anonKey } = validateEnv();
+const url = 'https://nqbxzvaksnnblpbhxsol.supabase.co';
+const anonKey = 'sb_publishable_XuXZLk-AaTQ9dEJPYvXVLg_MLEBew3p';
 
 export const supabase = createClient(url, anonKey, {
   auth: {
