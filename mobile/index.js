efcc3c6969d1cd0029e8d3fd7c81e0d1b74161b5
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-16 20:03:47 +0300

    Make redesigned home the Android app landing screen

--- mobile/index.js
+++ mobile/index.js
@@ -1,4 +1,13 @@
-import { registerRootComponent } from 'expo';
+import React,{useState} from 'react';
+import {View,StyleSheet} from 'react-native';
+import {registerRootComponent} from 'expo';
 import App from './App';
+import HomeRedesign from './src/HomeRedesign';
 
-registerRootComponent(App);
+function Root(){
+  const[showFullApp,setShowFullApp]=useState(false);
+  return <View style={s.root}>{showFullApp?<App/>:<HomeRedesign onOpenApp={()=>setShowFullApp(true)}/>}</View>;
+}
+
+const s=StyleSheet.create({root:{flex:1}});
+registerRootComponent(Root);
