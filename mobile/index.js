7c14de5c6f10a85e2b3199001d58160fc206afcd
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-19 19:47:05 +0300

    Remove duplicate HomeRedesign entry screen

--- mobile/index.js
+++ mobile/index.js
@@ -1,13 +1,4 @@
-import React,{useState} from 'react';
-import {View,StyleSheet} from 'react-native';
 import {registerRootComponent} from 'expo';
 import App from './App';
-import HomeRedesign from './src/HomeRedesign';
 
-function Root(){
-  const[showFullApp,setShowFullApp]=useState(false);
-  return <View style={s.root}>{showFullApp?<App/>:<HomeRedesign onOpenApp={()=>setShowFullApp(true)}/>}</View>;
-}
-
-const s=StyleSheet.create({root:{flex:1}});
-registerRootComponent(Root);
+registerRootComponent(App);
