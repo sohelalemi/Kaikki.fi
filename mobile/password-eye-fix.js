710441523d4f9d3218f35ca73eda8f6844536270
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-21 23:03:39 +0300

    Keep login password field fixed height

--- mobile/password-eye-fix.js
+++ mobile/password-eye-fix.js
@@ -17,6 +17,9 @@ function PasswordField(props){
       autoCapitalize:'none',
       autoCorrect:false,
       textContentType:'password',
+      multiline:false,
+      numberOfLines:1,
+      scrollEnabled:true,
     }),
     originalCreateElement(
       Pressable,
@@ -34,8 +37,8 @@ React.createElement=(type,props,...children)=>{
 };
 
 const styles=StyleSheet.create({
-  wrap:{position:'relative',justifyContent:'center'},
-  input:{paddingRight:52},
-  eye:{position:'absolute',right:12,width:38,height:38,borderRadius:19,alignItems:'center',justifyContent:'center'},
+  wrap:{position:'relative',justifyContent:'center',height:52,minHeight:52,maxHeight:52,width:'100%'},
+  input:{height:52,minHeight:52,maxHeight:52,paddingRight:52,paddingVertical:0,textAlignVertical:'center'},
+  eye:{position:'absolute',right:8,width:38,height:38,borderRadius:19,alignItems:'center',justifyContent:'center'},
   eyeText:{fontSize:20},
 });
