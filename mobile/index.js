b684197b6e51cb42d2a3feeccf61983732aab2fc
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-22 13:24:10 +0300

    Load Kaikki Diili feature

--- mobile/index.js
+++ mobile/index.js
@@ -2,6 +2,7 @@ import {registerRootComponent} from 'expo';
 import './layout-fix';
 import './password-eye-fix';
 import './image-zoom-fix';
+import './diili-fix';
 
 const App = require('./App').default;
 
