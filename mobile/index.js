778fae5d58eb725183a5a82350dbd2548d4ff0fe
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-22 14:10:21 +0300

    Show Kaikki Diili in Oma instead of listing cards

--- mobile/index.js
+++ mobile/index.js
@@ -2,7 +2,7 @@ import {registerRootComponent} from 'expo';
 import './layout-fix';
 import './password-eye-fix';
 import './image-zoom-fix';
-import './diili-fix';
+import './oma-diili-fix';
 
 const App = require('./App').default;
 
