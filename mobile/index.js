12ae55579b0148b7ccd82a3b951fc1a6869e71c5
Kaikki.fi <ayatalimi3366@gmail.com>
2026-08-23 15:30:24 +0300

    Enable functional mobile profile menu pages

--- mobile/index.js
+++ mobile/index.js
@@ -5,6 +5,7 @@ import './image-zoom-fix';
 import './diili-step-fix';
 import './diili-rejected-step-fix';
 import './oma-diili-v2';
+import './profile-menu-tools';
 import './settings-screen-fix';
 import './map-crash-fix';
 import './identity-verification';
