20510b50cab8130fb92a3225dd5019376fc8b915
Kaikki.fi <ayatalimi3366@gmail.com>
2026-09-03 20:52:45 +0300

    Load restored iPhone profile sections

--- mobile/index.js
+++ mobile/index.js
@@ -6,6 +6,7 @@ import './diili-step-fix';
 import './diili-rejected-step-fix';
 import './oma-diili-v2';
 import './profile-menu-tools';
+import './restore-profile-sections';
 import './support-screen-v2';
 import './profile-edit-screen-v2';
 import './map-crash-fix';
